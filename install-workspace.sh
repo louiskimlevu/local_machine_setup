3e513ec00e1794c39036332bf85746c95a313810
louiskim <louiskim.levu@gmail.com>
2022-05-25 03:53:17 +0800

    trim

--- install-workspace.sh
+++ install-workspace.sh
@@ -97,18 +97,15 @@ git config --global alias.sh 'show'
 git config --global alias.d 'diff'
 git config --global alias.co 'checkout'
 git config --global alias.c 'commit'
-
 ## diff-so-fancy
 # https://github.com/so-fancy/diff-so-fancy#usage
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 git config --global interactive.diffFilter "diff-so-fancy --patch"
 git config --global color.ui true
-
 git config --global color.diff-highlight.oldNormal    "red bold"
 git config --global color.diff-highlight.oldHighlight "red bold 52"
 git config --global color.diff-highlight.newNormal    "green bold"
 git config --global color.diff-highlight.newHighlight "green bold 22"
-
 git config --global color.diff.meta       "11"
 git config --global color.diff.frag       "magenta bold"
 git config --global color.diff.func       "146 bold"
