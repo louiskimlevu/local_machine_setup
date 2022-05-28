a70564e6a973259266fd66306f44afd6e7e39f39
louiskim <louiskim.levu@gmail.com>
2022-05-28 17:59:57 +0800

    removed git configs

--- install-workspace.sh
+++ install-workspace.sh
@@ -82,41 +82,11 @@ print_sep "Installing whatsapp"
 brew install --cask whatsapp
 print_sep "Installing bartender"
 brew install --cask bartender
-# packages
-# plugins
-# config
-
+print_sep "Installing numi"
+brew install --cask numi
+print_sep "Installing cleanshot"
+brew install --cask cleanshot
 #tmux
 #mkdir -p /usr/local/bin
 #sudo cp tmux/ide.sh /usr/local/bin/ide
 
-# karabiner
-#cp karabiner/*.json ~/.config/karabiner/assets/complex_modifications/
-
-## git config
-git config --global user.name "louiskim"
-git config --global user.email louiskim.levu@gmail.com
-## aliases
-git config --global alias.a 'add --verbose'
-git config --global alias.s 'status --untracked-files --short --branch'
-git config --global alias.l 'log --oneline'
-git config --global alias.sh 'show'
-git config --global alias.d 'diff'
-git config --global alias.co 'checkout'
-git config --global alias.c 'commit'
-## diff-so-fancy
-# https://github.com/so-fancy/diff-so-fancy#usage
-git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
-git config --global interactive.diffFilter "diff-so-fancy --patch"
-git config --global color.ui true
-git config --global color.diff-highlight.oldNormal    "red bold"
-git config --global color.diff-highlight.oldHighlight "red bold 52"
-git config --global color.diff-highlight.newNormal    "green bold"
-git config --global color.diff-highlight.newHighlight "green bold 22"
-git config --global color.diff.meta       "11"
-git config --global color.diff.frag       "magenta bold"
-git config --global color.diff.func       "146 bold"
-git config --global color.diff.commit     "yellow bold"
-git config --global color.diff.old        "red bold"
-git config --global color.diff.new        "green bold"
-git config --global color.diff.whitespace "red reverse"
