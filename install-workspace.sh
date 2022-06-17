2d15d96a29c974b233cb0bfd1199de290c001bfe
Louis Kim Le Vu <57093546+louiskimlevu@users.noreply.github.com>
2022-06-17 09:33:03 +0800

    Update install-workspace.sh

--- install-workspace.sh
+++ install-workspace.sh
@@ -66,6 +66,8 @@ print_sep "Installing kind"
 brew install kind
 print_sep "Installing k9s"
 brew install derailed/k9s/k9s
+print_sep "Installing kubectx"
+brew install kubectx
 
 print_sep "Installing GUI apps"
 print_sep "Installing iterm2"
