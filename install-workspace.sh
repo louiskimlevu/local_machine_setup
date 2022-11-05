e2896d4f67ae760e623fcb22ae0538a775c57354
Louis Kim Le Vu <57093546+louiskimlevu@users.noreply.github.com>
2022-11-06 00:35:17 +0800

    Update install-workspace.sh

--- install-workspace.sh
+++ install-workspace.sh
@@ -18,7 +18,9 @@ if [[ $(command -v brew) == "" ]]; then
     sudo chown -R $(whoami) /opt/homebrew
 fi
 
-print_sep "Installing Terminal tools"
+# Terminal
+print_sep "Installing iterm2"
+brew install --cask iterm2
 print_sep "Installing git"
 brew install git
 print_sep "Installing antigen"
@@ -37,6 +39,10 @@ print_sep "Installing exa"
 brew install exa
 print_sep "Installing z"
 brew install z
+print_sep "Installing yh"
+brew install yh
+print_sep "Installing jq"
+brew install jq
 print_sep "Installing tldr"
 brew install tldr
 print_sep "Installing diff-so-fancy"
@@ -48,62 +54,67 @@ brew install romkatv/powerlevel10k/powerlevel10k
 print_sep "Installing neovim"
 brew install neovim
 
-print_sep "Installing DevOps tools"
+# devops
 print_sep "Installing terraform"
 brew install hashicorp/tap/terraform-ls
 brew install terraform
 print_sep "Installing sentinel"
 brew install --cask sentinel
-print_sep "Installing node"
-brew install node
 print_sep "Installing awscli"
 brew install awscli
 print_sep "Installing docker"
 brew install --cask --no-quarantine docker
 print_sep "Installing kubectl"
 brew install kubectl
-print_sep "Installing yh"
-brew install yh
-print_sep "Installing jq"
-brew install jq
 print_sep "Installing kind"
 brew install kind
+print_sep "Installing Lens"
+brew install --cask lens
 print_sep "Installing k9s"
 brew install derailed/k9s/k9s
 print_sep "Installing kubectx"
 brew install kubectx
 print_sep "Installing helm"
 brew install helm
-print_sep "Installing Lens"
-brew install --cask lens
-print_sep "Installing iterm2"
-brew install --cask iterm2
+
+# Coding
+print_sep "Installing node"
+brew install node
+print_sep "Installing Postman"
+brew install --cask postman
 print_sep "Installing visual-studio-code"
 brew install --cask visual-studio-code
 print_sep "Installing Pycharm"
 brew install --cask pycharm-ce
+
+# Networking
 print_sep "Installing Wireshark"
 brew install --cask wireshark
 
-# productivity apps
+# Productivity
 print_sep "Installing google-chrome"
 brew install --cask google-chrome
 print_sep "Installing notion"
 brew install --cask notion
 print_sep "Installing slack"
 brew install --cask slack
+print_sep "Installing whatsapp"
+brew install --cask whatsapp
+
+# Mac only
 print_sep "Installing istat-menus"
 brew install --cask istat-menus
 print_sep "Installing clipy"
 brew install --cask clipy
+print_sep "Installing cleanshot"
+brew install --cask cleanshot
 print_sep "Installing karabiner-elements"
 brew install --cask karabiner-elements
 print_sep "Installing bettertouchtool"
 brew install --cask bettertouchtool
-print_sep "Installing whatsapp"
-brew install --cask whatsapp
-print_sep "Installing cleanshot"
-brew install --cask cleanshot
+
+
+# others
 print_sep "Installing ledger-live"
 brew install --cask ledger-live
 
