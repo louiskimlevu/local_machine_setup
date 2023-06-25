78176bb3e94d1a62639a839e2032b778ff3f5e82
louiskim <louiskim.levu@gmail.com>
2023-06-25 20:59:45 +0800

    update

--- install-workspace.sh
+++ install-workspace.sh
@@ -10,6 +10,9 @@ sudo -v
 # Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# XCode 
+xcode-select --install
+
 # brew
 print_sep "Installing Homebrew"
 if [[ $(command -v brew) == "" ]]; then
@@ -111,7 +114,7 @@ brew install --cask visual-studio-code
 print_sep "Installing Pycharm"
 brew install --cask pycharm-ce
 print_sep "Installing pyenv"
-brew install openssl readline sqlite3 xz zlib
+brew install openssl readline sqlite3 xz zlib bzip2
 curl https://pyenv.run | bash
 print_sep "Installing Postgresql"
 brew install postgresql
